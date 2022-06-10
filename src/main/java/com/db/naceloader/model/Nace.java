2912fedb311f27e77e2ec4768a2691a07bbdf910
mylifemyway <kumbhkar.ritesh@gmail.com>
2022-06-10 12:08:19 +0530

    First Test 404

---
+++ src/main/java/com/db/naceloader/model/Nace.java
@@ -0,0 +1,13 @@
+package com.db.naceloader.model;
+
+public class Nace {
+    private Integer order;
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+}
--- src/test/java/com/db/naceloader/ApplicationTests.java
+++ src/test/java/com/db/naceloader/ApplicationTests.java
@@ -1,13 +1,29 @@
 package com.db.naceloader;
 
+import com.db.naceloader.model.Nace;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void getNaceDetails() {
+		final ResponseEntity<Nace> responseEntity = restTemplate.getForEntity("/nace/getNaceDetails/398481", Nace.class);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(responseEntity.getBody().getOrder()).isEqualTo(398481);
 	}
 
 }
